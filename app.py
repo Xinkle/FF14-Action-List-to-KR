31168a9284490324ad60acde4dac63e3112abafe
Xinkle <cswcsy@gmail.com>
2018-12-11 22:06:57 +0900

    Implement all function

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 import pandas
 
+ENG_Action_DF = pandas.read_csv('Action.en-US.csv')
+KOR_Action_DF = pandas.read_csv('SkillList_KR.txt', sep="|", names=['idx', 'name'])
+
+for (index, row) in KOR_Action_DF.iterrows():
+    dec_index = int(row[0], 16)
+    ENG_Action_DF['0'][dec_index+2] = row[1]
+
+ENG_Action_DF.to_csv('Action.ko-KR.csv', index=False)
